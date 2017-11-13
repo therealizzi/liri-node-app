5807fc44f9189fb9ccb123c55c8bd27da3c66470
TheRealIzzi <israel.peck@gmail.com>
2017-11-13 13:32:50 -0600

    Setting up the initial file structure for Liri

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'FIrUySf2I77dnglRpxqXKijT9',
+  consumer_secret: 'f09KbmFxvOca0DEOPLd7JgGcHUrsKc3IF0QsMEfZJo6ebK11yU',
+  access_token_key: '930151196790284288-Qs6Zo1ErVHb40UIXd7rILVhB3pWaEbN',
+  access_token_secret: 'dl7lwrCcd9p1ZOeJONHbCIsqc78CpnnLHBshMtYsH0XX6',
+}
+
+module.exports = twitterKeys;
