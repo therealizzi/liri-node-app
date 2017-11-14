17e75682aa1648edce2710ef2d67848ff222e19e
TheRealIzzi <israel.peck@gmail.com>
2017-11-14 12:57:10 -0600

    The OMDB request is setup and serving data, status ~70%

--- liri.js
+++ liri.js
@@ -1,38 +1,49 @@
-//Here are the global variables
+//Global variables
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var Request = require("request")
 var keys = require('./keys.js');
 var user_input = [];
 
-//This collects the arguments and pushes them to user_input array
-for (var i = 2; i < process.argv.length; i++){
+//Creating user_input array
+for (var i = 3; i < process.argv.length; i++){
 	user_input.push(process.argv[i].trim());
 };
 
-//This is twitter's client variable
-var client = new Twitter ({
+
+//=================== client variables ====================
+
+
+//Twitter
+var twitter = new Twitter ({
 	consumer_key: keys.consumer_key,
 	consumer_secret: keys.consumer_secret,
 	access_token_key: keys.access_token_key,
 	access_token_secret: keys.access_token_secret
 });
 
-//This is the Spotify variable
+//Spotify
 var spotify = new Spotify ({
 	id: '15233c110ca34cca9ec892298c351ca9',
 	secret: '95cbe30033be459bb0bb989ac4b3c252'
 });
 
-//twitter's get parameters
+
+// //=================== twitter call ====================
+
+
+//Argv validation
+if(process.argv[2] === "my-tweets"){
+	
+//Setting twitter parameters
 var params = {screen_name: 'zeuspowerinc'};
 
-//If function to activate twitter client
-if(user_input === "my-tweets"){
-	
-	//Calling client (twitter) to get tweets
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	//Calling to twitter
+	twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if(!error) {
 			for (var i = 0; i < 20; i++){
+
+				//Print data
 				console.log(tweets[i].text);
 				console.log(tweets[i].created_at);		
 			}
@@ -40,14 +51,39 @@ if(user_input === "my-tweets"){
 	});
 }
 
-//This uses the Spotify 'search' method to pull data, based on user_input
-spotify.search({ type: 'track', query: user_input, limit: 1}, function(err, data){
-	if (err) {
-		return console.log('Error occured: '+ err);
-	}
-console.log("Artist(s): "+data.tracks.items[0].album.artists[0].name);
-console.log("Song: "+data.tracks.items[0].name);
-console.log("Preview: "+data.tracks.items[0].album.href);
-console.log("Album: "+data.tracks.items[0].album.name);
-});
+
+// //=================== spotify call ====================
+
+
+//Argv validation
+if(process.argv[2] === "spotify-this-song"){
+
+	//Use spotify's 'search' method
+	spotify.search({ type: 'track', query: user_input, limit: 1}, function(err, data){
+		if (err) {
+			return console.log('Error occured: '+ err);
+		}
+	//Print data
+	console.log("Artist(s): "+data.tracks.items[0].album.artists[0].name);
+	console.log("Song: "+data.tracks.items[0].name);
+	console.log("Preview: "+data.tracks.items[0].album.href);
+	console.log("Album: "+data.tracks.items[0].album.name);
+	});
+}
+
+
+//=================== omdb call ====================
+
+//OMDB
+// var omdb = new Request("http://www.omdbapi.com/?apikey=40e9cece&?t="+user_input);
+
+//Argv validation
+if(process.argv[2] === "movie-this") {
+
+	new Request("http://www.omdbapi.com/?t="+user_input+"&apikey=40e9cece&", function(error, response, body) {
+		if (!error && response.statusCode === 200){
+			console.log(body);
+		}
+	});
+}
 
