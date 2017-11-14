c3a481fb1e27c041e6ad2ed82b27cc830d6bb637
TheRealIzzi <israel.peck@gmail.com>
2017-11-14 11:23:36 -0600

    Spotify is setup and working, project is currently ~65%

--- liri.js
+++ liri.js
@@ -1,7 +1,13 @@
 //Here are the global variables
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
-var user_input = process.argv[2];
+var user_input = [];
+
+//This collects the arguments and pushes them to user_input array
+for (var i = 2; i < process.argv.length; i++){
+	user_input.push(process.argv[i].trim());
+};
 
 //This is twitter's client variable
 var client = new Twitter ({
@@ -11,7 +17,13 @@ var client = new Twitter ({
 	access_token_secret: keys.access_token_secret
 });
 
-//Twitter's get parameters
+//This is the Spotify variable
+var spotify = new Spotify ({
+	id: '15233c110ca34cca9ec892298c351ca9',
+	secret: '95cbe30033be459bb0bb989ac4b3c252'
+});
+
+//twitter's get parameters
 var params = {screen_name: 'zeuspowerinc'};
 
 //If function to activate twitter client
@@ -27,3 +39,15 @@ if(user_input === "my-tweets"){
 		}
 	});
 }
+
+//This uses the Spotify 'search' method to pull data, based on user_input
+spotify.search({ type: 'track', query: user_input, limit: 1}, function(err, data){
+	if (err) {
+		return console.log('Error occured: '+ err);
+	}
+console.log("Artist(s): "+data.tracks.items[0].album.artists[0].name);
+console.log("Song: "+data.tracks.items[0].name);
+console.log("Preview: "+data.tracks.items[0].album.href);
+console.log("Album: "+data.tracks.items[0].album.name);
+});
+
