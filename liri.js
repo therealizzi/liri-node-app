6c0d2a3aa0cdc8a219ca22bd6730ec33bef8adf0
TheRealIzzi <israel.peck@gmail.com>
2017-11-15 15:10:20 -0600

    Redacted code to operate as functions, now it all works, ~95%

--- liri.js
+++ liri.js
@@ -2,27 +2,16 @@
 //=================== global ====================
 
 
-//Variables
+//Node packages 
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var Request = require("request");
 var fs = require("fs");
 var keys = require('./keys.js');
+
+//Variables
 var operator = process.argv[2];
 var user_input = [];
-
-//User input verification
-if(operator === "movie-this" && process.argv[3] === undefined){
-	user_input.push("Mr. Nobody")
-} else 
-
-if (operator === "spotify-this-song" && process.argv[3] === undefined){
-	user_input.push("The Sign Ace of Base")
-} else 
-
-for (var i = 3; i < process.argv.length; i++){
-	user_input.push(process.argv[i].trim());
-};
 
 
 //=================== clients ====================
@@ -43,16 +32,61 @@ var spotify = new Spotify ({
 });
 
 
-//=================== twitter ====================
+//=================== input verification ====================
 
 
-//Tweet operator validation
-if(operator === "my-tweets"){
+//Handle movie blanks
+if(operator === "movie-this" && process.argv[3] === undefined){
+
+	//Use default search
+	user_input.push("Mr. Nobody")
+} else 
+
+//Handle song blanks
+if (operator === "spotify-this-song" && process.argv[3] === undefined){
 	
-//Setting twitter parameters
-var params = {screen_name: 'zeuspowerinc'};
+	//Use default search
+	user_input.push("The Sign Ace of Base")
+} else 
 
-	//Calling twitter using 'get' method
+//Handle non-blanks
+for (var i = 3; i < process.argv.length; i++){
+
+	//Load arguments
+	user_input.push(process.argv[i].trim());
+};
+
+//Tweet operator validation & function call
+if(operator === "my-tweets"){
+	myTweets()
+};
+
+//Spotify operator validation & function call
+if(operator === "spotify-this-song"){
+	spotifyThisSong(user_input)
+};
+
+//OMDB operator validation & function call
+if(operator === "movie-this") {
+	movieThis(user_input)
+};
+
+//FS operator validation & function call
+if(operator === "do-what-it-says") {
+	doWhatItSays()
+}
+
+
+//=================== functions ====================
+
+
+//Twitter  
+function myTweets() {	
+
+	//Setting twitter get parameters
+	var params = {screen_name: 'zeuspowerinc'};
+
+	//Calling using 'get' method
 	twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if(!error) {
 			for (var i = 0; i < 20; i++){
@@ -65,62 +99,28 @@ var params = {screen_name: 'zeuspowerinc'};
 	});
 }
 
+//Spotify  
+function spotifyThisSong() {
 
-//=================== fs ====================
-
-
-//FS pperator validation
-if(operator === "do-what-it-says") {
-
-	//FS read method for target file "random.txt"
-	fs.readFile("random.txt", 'utf8', function(error, data) {
-		if(error) {
-			return console.log(error);
-		} 
-		console.log(data);
-	
-		var dataArray = data.replace(","," ");
-	
-		console.log(dataArray);
-
-		global.operator = dataArray[0];
-		user_input.push(dataArray[1]);
-		});
-	};
-
-//// HERE IS THE ISSUE: the operator is still "do-what-it-says", it isn't changing //////
-
-// console.log(operator); 
-// console.log(user_input);
-
-
-//=================== spotify ====================
-
-
-//Spotify operator validation
-if(operator === "spotify-this-song"){
-
-	//Calling spotify using 'search' method
+	//Setting spotify search parameters
 	spotify.search({ type: 'track', query: user_input, limit: 1}, function(err, data){
 		if (err) {
 			return console.log('Error occured: '+ err);
 		}
+
 		//Print data
 		console.log("Artist(s): "+data.tracks.items[0].album.artists[0].name+ '\n'
 					+"Song: "+data.tracks.items[0].name+ '\n'
 					+"Preview: "+data.tracks.items[0].album.href+ '\n'
-					+"Album: "+data.tracks.items[0].album.name);
+					+"Album: "+data.tracks.items[0].album.name
+		);
 	});
 }
 
+//OMDB 
+function movieThis() {
 
-//=================== omdb ====================
-
-
-//OMDB operator validation
-if(operator === "movie-this") {
-
-	//OMDB using 'request' method
+	//Setting ombd request parameters
 	new Request("http://www.omdbapi.com/?t="+user_input+"&apikey=40e9cece&", function(error, response, data) {
 		if (!error && response.statusCode === 200){
 
@@ -140,4 +140,35 @@ if(operator === "movie-this") {
 	});
 }
 
+//FS
+function doWhatItSays() {
+
+	//FS read method for target file "random.txt"
+	fs.readFile("random.txt", 'utf8', function(error, data) {
+		if(error) {
+			return console.log(error);
+		} 
+
+		//Break FS data string into array
+		var dataArray = data.split(",");
+
+		//Define local variables
+		operator = dataArray[0];
+		user_input.push(dataArray[1]);
+
+		//Call appropriate function
+		if(operator === "spotify-this-song"){
+			spotifyThisSong(user_input)
+		} else
+
+		if(operator === "movie-this"){
+			movieThis(user_input)
+		} else
+
+		if(operator === "my-tweets"){
+			myTweets()
+		}
+
+	});
+};
 
